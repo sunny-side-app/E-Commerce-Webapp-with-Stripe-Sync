eba7b7512037f44734ece2b472c0321a49da289a
haru <kawachigenji95@gmail.com>
2024-11-03 02:40:53 +0900

    定義済みの関数をimportするように変更

--- src/clothes_shop/views/checkout_views.py
+++ src/clothes_shop/views/checkout_views.py
@@ -1,30 +1,15 @@
 import logging
 
 from rest_framework import status
-from rest_framework.exceptions import APIException, NotFound
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from clothes_shop.models import Product
 from clothes_shop.serializers import CheckoutListSerializer, CheckoutSerializer
 from clothes_shop.services.stripe_service import CheckoutData, StripeService
+from clothes_shop.views.product_views import get_product
 
 logger = logging.getLogger(__name__)
 striep_service = StripeService()
-
-
-def get_product(product_id) -> Product:
-    try:
-        product = Product.objects.get(pk=product_id)
-        return product
-    except Product.DoesNotExist:
-        errMsg = f"指定されたID {product_id} に紐づく製品が存在しません。"
-        logger.error(errMsg)
-        raise NotFound(detail=errMsg)
-    except Exception as e:
-        errMsg = f"想定外のエラーが発生しました: {str(e)}"
-        logger.error(errMsg)
-        raise APIException(detail=errMsg)
 
 
 class StripeCheckoutView(APIView):
