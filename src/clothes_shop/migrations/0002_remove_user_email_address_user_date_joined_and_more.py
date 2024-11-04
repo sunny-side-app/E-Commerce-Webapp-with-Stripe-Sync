4f4e3ecc30f63acc19f6174ece1d5b27f56a75a6
yadon <doinkya0@gmail.com>
2024-11-04 14:30:34 +0000

    Fix: migration file(002)

--- src/clothes_shop/migrations/0002_remove_user_email_address_user_date_joined_and_more.py
+++ src/clothes_shop/migrations/0002_remove_user_email_address_user_date_joined_and_more.py
@@ -3,12 +3,11 @@
 import django.utils.timezone
 from django.db import migrations, models
 
-
 class Migration(migrations.Migration):
 
     dependencies = [
         ('auth', '0012_alter_user_first_name_max_length'),
-        ('clothes_shop', '0003_rename_product_id_review_product_and_more'),
+        ('clothes_shop', '0001_initial'),
     ]
 
     operations = [
