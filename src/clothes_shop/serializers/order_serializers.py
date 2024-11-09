c4c54fc165774b678bf32920f03093a6544558c9
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-11-09 14:25:09 +0900

    Order関連APIをAPIViewに＋返り値の中にユーザー情報

--- src/clothes_shop/serializers/order_serializers.py
+++ src/clothes_shop/serializers/order_serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 from clothes_shop.models.order import Order, OrderItem
+from clothes_shop.models.user import User
+from clothes_shop.serializers.user_serializers import UserSerializer
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
@@ -11,10 +13,29 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 class OrderSerializer(serializers.ModelSerializer):
     order_items = OrderItemSerializer(many=True)
+    user = serializers.SerializerMethodField(read_only=True)
+    user_pk = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), write_only=True)
 
     class Meta:
         model = Order
-        fields = ("id", "user", "order_date", "order_status", "total_price", "order_items")
+
+        fields = (
+            "id",
+            "user",
+            "order_date",
+            "order_status",
+            "total_price",
+            "order_items",
+            "user_pk",
+        )
+
+    def get_user(self, obj: Order):
+        return UserSerializer(obj.user).data
+
+    def create(self, validated_data):
+        user = validated_data.pop("user_pk")
+        order = Order.objects.create(user=user, **validated_data)
+        return order
 
 
 class OrderListSerializer(serializers.ListSerializer):
