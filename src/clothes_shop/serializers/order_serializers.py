bbea6209312cd75432565563378865796d93f797
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-11-11 12:15:46 +0900

    OrderDetailView　product詳細追加＋権限調整

--- src/clothes_shop/serializers/order_serializers.py
+++ src/clothes_shop/serializers/order_serializers.py
@@ -1,18 +1,27 @@
 from rest_framework import serializers
 
 from clothes_shop.models.order import Order, OrderItem
+from clothes_shop.models.product import Product
 from clothes_shop.models.user import User
+from clothes_shop.serializers.product_serializers import ProductSerializer
 from clothes_shop.serializers.user_serializers import UserSerializer
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    product = serializers.SerializerMethodField(read_only=True)
+    product_pk = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(), write_only=True)
+
     class Meta:
         model = OrderItem
-        fields = ("order", "product", "quantity", "unit_price")
+        fields = ("order", "product", "quantity", "unit_price", "product_pk")
+
+    def get_product(self, obj: Order):
+        return ProductSerializer(obj.product).data
 
 
 class OrderSerializer(serializers.ModelSerializer):
     order_items = OrderItemSerializer(many=True)
+
     user = serializers.SerializerMethodField(read_only=True)
     user_pk = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), write_only=True)
 
