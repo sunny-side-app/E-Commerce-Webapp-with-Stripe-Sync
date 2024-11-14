523c5397f1654f09996b7edad3d72be37a1f5d62
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-11-14 09:21:39 +0900

    loginCheckのエンドポイント

--- src/clothes_shop/views/user_views.py
+++ src/clothes_shop/views/user_views.py
@@ -2,8 +2,10 @@ import logging
 
 from rest_framework import generics, permissions, status
 from rest_framework.exceptions import APIException, NotFound, ValidationError
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from clothes_shop.models.user import User
@@ -87,3 +89,24 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class CheckAccessAndAdminView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+
+        check_admin = request.data.get("check_admin")
+
+        if check_admin is None:
+            check_admin = False
+        user = request.user
+
+        if check_admin and not user.is_staff:
+            return Response(
+                {"message": "管理者権限が必要です", "result": False},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        return Response({"result": True}, status=status.HTTP_200_OK)
